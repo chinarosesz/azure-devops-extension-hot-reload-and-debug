8d34580a7d30b34e425700ca16293d5facb1a43c
Lila Tran <litra@microsoft.com>
2020-03-25 14:18:23 -0700

    Reorg

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 call npm install
 call webpack --mode development
-call tfx extension create --manifest-globs vss-extension.json --overrides-file configs/dev.json
-call tfx extension create --manifest-globs vss-extension.json --overrides-file configs/release.json
+call tfx extension create --manifest-globs vss-extension.json --overrides-file vss-extension-dev.json
+call tfx extension create --manifest-globs vss-extension.json --overrides-file vss-extension-release.json
 call webpack-dev-server --mode development

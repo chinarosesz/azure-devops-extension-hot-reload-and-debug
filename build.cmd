35f0ddd05c5385186f1c0f74e2f75e4c73585285
Lila Tran <litra@microsoft.com>
2020-04-15 12:24:34 -0700

    Cleaning up

--- build.cmd
+++
@@ -1,5 +0,0 @@
-call npm install
-call webpack --mode development
-call tfx extension create --manifest-globs vss-extension.json --overrides-file vss-extension-dev.json
-call tfx extension create --manifest-globs vss-extension.json --overrides-file vss-extension-release.json
-call webpack-dev-server --mode development
